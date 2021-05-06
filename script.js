36ec2b80d127db3aec525e33df07b955f4a3ab79
7vm85 <66986583+7vm85@users.noreply.github.com>
2021-05-05 20:47:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let a = prompt("Hi are you interested in coding or programming?")
+if(a === "yes") {
+	alert("That is fine so you will read this article and decide what language you want to learn")
+} else if(a === "no") {
+	alert("You can read this aricle try to learn some laguages and be sure you want to learn or no")
+}
